8f502698973ed04c8314fd861b746df58b3a5dd6
Sym Roe <sym.roe@talusdesign.co.uk>
2013-12-14 18:50:53 +0000

    fab file updates

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import run, env, sudo, cd
+from fabric.api import run, env, sudo, cd, prefix
 
 env.hosts = ['remakery.talusdesign.co.uk:7822',]
 # env.user = "remakery"
@@ -12,8 +12,14 @@ def configure_nginx():
     update_from_git()
     config_file = "%s%s/nginx" % (env.depoy_path, env.config_path)
     sudo('cp -f %s /etc/nginx/sites-available/' % config_file)
+    sudo('ln -s /etc/nginx/sites-available/nginx /etc/nginx/sites-enabled/remakery')
     service('nginx')
 
 def update_from_git():
     with cd(env.depoy_path):
         run("git pull origin master")
+
+def pip_install():
+    with cd(env.depoy_path):
+        with prefix('source env/bin/activate'):
+            run("pip install -r requirements.txt")
